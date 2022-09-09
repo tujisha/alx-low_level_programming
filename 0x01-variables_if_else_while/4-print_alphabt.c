4baa3c1e87f3c9826928d9094cf3fa699b2bd053
tujisha <tujigerdas@gmail.com>
2022-09-09 13:02:36 -0700

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*  main - prints the alphabet in lowercase.
+(*
+*  Description: 'using the main function'
+*  This program prints "Programming is positive, zero or negative
+*  Return: 0 on success
+**/
+int main(void)
+{
+	char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch != 'e'; && ch != 'q')
+{
+putchar(ch);
+}
+}
+putchar('\n');
+	return (0);
+}
+
