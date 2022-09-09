30c07baf1a169bfdc36436c5748fa0f5c45bddba
tujisha <tujigerdas@gmail.com>
2022-09-09 13:58:57 -0700

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,12 @@ int main(void)
 	int c;
 for (c = 48; c <= 57; c++)
 {
-         putchar(c);
-         if (c != 57)
-         {
-                  putchar(' , ');
-                  putchar('   ');
-          }
+putchar(c);
+if (c != 57)
+{
+putchar(' , ');
+putchar('   ');
+}
 }
 putchar('\n');
 	return (0);
