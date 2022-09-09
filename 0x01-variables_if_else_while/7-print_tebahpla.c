39d95d72c3ad258ca13299523022d4734710ab9f
tujisha <tujigerdas@gmail.com>
2022-09-09 13:32:34 -0700

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*  main - prints the alphabet in lowercase.
+(*
+*  Description: 'using the main function'
+*  This program prints "Programming is positive, zero or negative
+*  Return: 0 on success
+**/
+int main(void)
+{
+	char c;
+for (c = 'z' ; c >= 'a' ; c--)
+{
+putchar(c);
+}
+putchar('\n');
+	return (0);
+}
+
