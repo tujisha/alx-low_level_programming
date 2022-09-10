fdebfef092865862c6c7e0c48599a68c87a4a866
tujisha <tujigerdas@gmail.com>
2022-09-10 11:41:53 -0700

    print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*  main - prints the alphabet in lowercase.
-(*
-*  Description: 'using the main function'
-*  This program prints "Programming is positive, zero or negative
-*  Return: 0 on success
-**/
+* main - prints the alphabet in lowercase.
+*
+* Description: 'using the main function'
+*
+* Return: 0 on success
+*/
 int main(void)
 {
 	int i;
