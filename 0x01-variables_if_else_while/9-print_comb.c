293f01c6af2b5976978c37680a48c3f03ab419a5
tujisha <tujigerdas@gmail.com>
2022-09-10 12:23:13 -0700

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 /**
-*  main - prints the alphabet in lowercase.
-(*
-*  Description: 'using the main function'
-*  This program prints "Programming is positive, zero or negative
-*  Return: 0 on success
-**/
+* main - prints the alphabet in lowercase.
+*
+* Description: 'using the main function'
+*
+* Return: 0 on success
+*/
 int main(void)
 {
-	int c;
-for (c = 48; c <= 57; c++)
+int d;
+for (d = '0'; d <= '9'; d++)
 {
-putchar(c);
-if (c != 57)
+putchar(d);
+if (d != '9');
 {
 putchar(' , ');
 putchar('   ');
 }
 }
 putchar('\n');
-	return (0);
+return (0);
 }
 
