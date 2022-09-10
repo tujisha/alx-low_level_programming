d0a8d23bc7aba7e95956de6c40deb4afe9dc51b4
tujisha <tujigerdas@gmail.com>
2022-09-10 11:11:02 -0700

    print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,16 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
 /**
-*  main - prints the alphabet in lowercase.
-(*
-*  Description: 'using the main function'
-*  This program prints "Programming is positive, zero or negative
-*  Return: 0 on success
-**/
+* main - Entry point
+* Description: 'using the main function.'
+*
+* Return: 0 on success
+*/
 int main(void)
 {
 	char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-putchar(ch);
-}
-for (ch = 'A' ; ch <= 'Z' ; ch++)
 {
 putchar(ch);
 }
