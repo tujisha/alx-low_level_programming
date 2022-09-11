b9517e8b40aee4450e5f827f492303d2db1851f4
tujisha <tujigerdas@gmail.com>
2022-09-11 08:56:36 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* Description: main - Prints out the last part.
+* Return: 1 if success.
+*/
+int main(void)
+{
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+return (1);
+}
