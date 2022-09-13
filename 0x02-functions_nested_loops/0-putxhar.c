3b5c56915f12beed66326534101037f9c61980c8
tujisha <tujigerdas@gmail.com>
2022-09-13 05:54:43 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putxhar.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include<stdio.h>
+/**
+* main - Entry point
+* Return:0
+*/
+int main(void)
+{
+char c[10] = "Holberton";
+int i = 0;
+while (i < 9)
+{
+putchar(c[i]);
+i++;
+}
+putchar('\n');
+return (0);
+}
