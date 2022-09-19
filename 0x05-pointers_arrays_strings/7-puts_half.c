15f0639d7891249e5877ef9d44a902f041f57fed
tujisha <tujigerdas@gmail.com>
2022-09-19 09:09:15 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* puts_half - print second half of a string
+* @str: char array string type
+* Description: If odd nmr of chrs, print (length - 1) / 2
+*/
+void puts_half(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+;
+i++;
+for (i /= 2; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
