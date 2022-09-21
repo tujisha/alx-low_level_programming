d15ee41a07332dc4e74f5a5ed400dddf54877c24
tujisha <tujigerdas@gmail.com>
2022-09-21 10:15:05 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,42 +1,25 @@
 #include "main.h"
 /**
-* rot13 - Encodes a string using rot13.
-* @str: The string to be encoded.
-*
-* Return: A pointer to the encoded string.
+* *rot13 - encodes a string using rot13.
+* @s: int type array pointer
+* Return: encoded
 */
-char *rot13(char *str)
+char *rot13(char *s)
 {
-int indx1 = 0, indx2;
-char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 
-                              'G', 'H', 'I', 'J', 'K', 'L', 
-                              'M', 'N', 'O', 'P', 'Q', 'R', 
-                              'S', 'T', 'U', 'V', 'W', 'X', 
-                              'Y', 'Z', 'a', 'b', 'c', 'd', 
-                              'e', 'f', 'g', 'h', 'i', 'j', 
-                              'k', 'l', 'm', 'n', 'o', 'p', 
-                              'q', 'r', 's', 't', 'u', 'v', 
-                              'w', 'x', 'y', 'z'};
-char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 
-                              'T', 'U', 'V', 'W', 'X', 'Y', 
-                              'Z', 'A', 'B', 'C', 'D', 'E', 
-                              'F', 'G', 'H', 'I', 'J', 'K', 
-                              'L', 'M', 'n', 'o', 'p', 'q', 
-                              'r', 's', 't', 'u', 'v', 'w', 
-                              'x', 'y', 'z', 'a', 'b', 'c', 
-                              'd', 'e', 'f', 'g', 'h', 'i', 
-                              'j', 'k', 'l', 'm'};
-while (str[indx1])
+int i, ii;
+char input[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (i = 0; s[i] != '\0'; i++)
 {
-for (indx2 = 0; indx2 < 52; indx2++)
+for (ii = 0; ii < 54; ii++)
 {
-if (str[indx1] == alphabet[indx2])
+if (((s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' && s[i] >= 'A')) 
+                 && s[i] == input[ii])
 {
-str[indx1] = rot13key[indx2];
+s[i] = output[ii];
 break;
 }
 }
-indx1++;
 }
-return (str);
+return (s);
 }
