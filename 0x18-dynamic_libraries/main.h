b816f0803b82f750ded188e7d76a7a58b0ac2c78
Tuji <tujigerdas.com>
2023-06-13 08:42:11 -0700

    main

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,10 +24,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int add(int a, int b);
-int sub(int a, int b);
-int mul(int a, int b);
-int mod(int a, int b);
-extern div_t div(int __numer, int __denom);
 
-#endif /* MAIN_H */
+#endif /* HOLBERTON_H */
